8bdc4f9c74c9c802833a4b894480d51027615ad6
Josh Nussbaum <joshnuss@gmail.com>
2019-03-12 01:07:43 -0400

    Initial commit

---
+++ lib/combinatorics.ex
@@ -0,0 +1,18 @@
+defmodule Combinatorics do
+  @moduledoc """
+  Documentation for Combinatorics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Combinatorics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/combinatorics_test.exs
@@ -0,0 +1,8 @@
+defmodule CombinatoricsTest do
+  use ExUnit.Case
+  doctest Combinatorics
+
+  test "greets the world" do
+    assert Combinatorics.hello() == :world
+  end
+end
